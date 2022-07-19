2c3e17e2338437572b829a5272acf4212dd8e4dd
Your Name <you@example.com>
2022-07-19 20:54:28 +0200

    ej 1 y 2

---
+++ ej2.js
@@ -0,0 +1,7 @@
+//  programa que pida 3 números y escriba en la pantalla el mayor de los tres
+
+function app() {
+    let number1 = document.getElementById('number1');
+    console.log(number1);
+    
+}
